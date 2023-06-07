2e485be372d0942b0ce4999ba207e58b8ee388e8
Denys Malanichev <deniskirillov228@gmail.com>
2023-06-07 13:38:01 +0300

    Added Hotels API endpoint

---
+++ HotelListing.API/Repository/HotelsRepository.cs
@@ -0,0 +1,19 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+
+namespace HotelListing.API.Repository
+{
+	public class HotelsRepository : GenericRepository<Hotel>, IHotelsRepository
+	{
+		private readonly HotelListingDbContext _context;
+
+		public HotelsRepository(HotelListingDbContext context) : base(context)
+        {
+			_context = context;
+		}
+        public async Task<Hotel> GetDetails(int id)
+		{
+			return await _context.Hotels.FindAsync(id);
+		}
+	}
+}
